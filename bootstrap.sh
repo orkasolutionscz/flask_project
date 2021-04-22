09f9ca53794868a85412761def8105087f089ff7
orkasolutionscz <fait@orkasolutions.cz>
2021-04-22 23:16:21 +0000

    startovaci skript pro VM

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+# install Python3.6
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.6
+
+# update alternetives
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+
+# install pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+
+# install other dev-tools
+apt install -y git python-dev
+
+# install virtualenv
+sudo pip install virtualenv
