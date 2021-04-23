82ca411f524da6ba439fe7abb3fad02a554a7aed
orkasolutionscz <fait@orkasolutions.cz>
2021-04-23 08:01:52 +0000

    nova instalace

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://vagrantcloud.com/search.
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "ubuntu/focal64"
 
   config.vm.provider "virtualbox" do |v|
     v.memory = 2048
@@ -68,8 +68,5 @@ Vagrant.configure("2") do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Ansible, Chef, Docker, Puppet and Salt are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  # config.vm.provision "shell", inline: <<-SHELL
-  #   apt-get update
-  #   apt-get install -y apache2
-  # SHELL
+  config.vm.provision :shell, :path => "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
+apt update && apt upgrade -y
 # install Python3.6
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install python3.6
+# add-apt-repository ppa:deadsnakes/ppa
+# apt update
+# apt install python3.8
 
 # update alternetives
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 
 # install pip
 wget https://bootstrap.pypa.io/get-pip.py
@@ -13,6 +14,7 @@ python3 get-pip.py
 
 # install other dev-tools
 apt install -y git python-dev
+apt install mc -y
 
 # install virtualenv
 sudo pip install virtualenv
