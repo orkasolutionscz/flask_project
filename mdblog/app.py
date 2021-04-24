bbe88b558c4712436a97fec49c9892d1f44a7077
jiri.fait <fait@orkasolutions.cz>
2021-04-24 23:59:54 +0200

    Pridani dalsich podstranek a uprava jinja templates

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,9 +5,17 @@ flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def view_welcome_page():
-    return render_template("welcome_page.jinja", text='ahoj debile, jak je<')
+    return render_template("welcome_page.jinja")
 
-@flask_app.route("/about")
+@flask_app.route("/about/")
 def view_about_page():
-    return render_template("about_page.jinja", text='ahoj debile, jak je<')
+    return render_template("about_page.jinja")
+
+@flask_app.route("/admin/")
+def view_admin_page():
+    return render_template("admin_page.jinja")
+
+@flask_app.route("/article/")
+def view_article_page():
+    return render_template("article_page.jinja")
 
--- run.py
+++ run.py
@@ -4,4 +4,3 @@ if __name__ == "__main__":
     debug = True
     host = "0.0.0.0"
     flask_app.run(host, debug=debug)
-
