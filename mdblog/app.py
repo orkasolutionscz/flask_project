491f0632110a46a3c7247596008e28816e662f69
jiri.fait <fait@orkasolutions.cz>
2021-04-25 01:03:17 +0200

    oprava funkce login

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 from .database import articles
 
 flask_app = Flask(__name__)
@@ -23,5 +26,21 @@ def view_articles_page():
 @flask_app.route("/article/<int:art_id>")
 def view_article_page(art_id):
     article = articles.get(art_id)
-    return render_template("article_page.jinja", article=article)
+    if article:
+        return render_template("article_page.jinja", article=article)
+    return render_template("article_not_found.jinja", art_id=art_id)
 
+
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.jinja")
+
+
+@flask_app.route("/login/", methods=["POST"])
+def login_user():
+    username= request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password == "admin":
+        return redirect(url_for("view_admin_page"))
+    else:
+        return redirect(url_for("view_login"))
