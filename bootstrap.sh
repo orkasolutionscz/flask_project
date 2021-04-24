2edaad3aaf0afe7005a64c7c5a59adacb353c040
orkasolustionscz <fait@orkasolustions.cz>
2021-04-24 19:11:57 +0000

    Pridani default souboru

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,23 @@
-apt update && apt upgrade -y
 # install Python3.6
-# add-apt-repository ppa:deadsnakes/ppa
-# apt update
-# apt install python3.8
+add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.6 -y
 
 # update alternetives
-#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
 
 # install pip
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 
 # install other dev-tools
-apt install -y git python-dev
-apt install mc -y
+sudo apt install -y git python-dev
+sudo apt install mc -y
 
 # install virtualenv
 sudo pip install virtualenv
+
+# Nastaveni gitu
+git config --global user.name "orkasolutionscz"
+git config --global user.email "fait@orkasolutions.cz"
