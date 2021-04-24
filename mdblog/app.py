5d8cdd501b937ffc4aca9a18a3e2ec3bf58db75f
jiri.fait <fait@orkasolutions.cz>
2021-04-25 00:31:49 +0200

    Refactor _main.jinja

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from .database import articles
 
 flask_app = Flask(__name__)
 
@@ -16,6 +17,11 @@ def view_admin_page():
     return render_template("admin_page.jinja")
 
 @flask_app.route("/article/")
-def view_article_page():
-    return render_template("article_page.jinja")
+def view_articles_page():
+    return render_template("articles_page.jinja", articles=articles.items())
+
+@flask_app.route("/article/<int:art_id>")
+def view_article_page(art_id):
+    article = articles.get(art_id)
+    return render_template("article_page.jinja", article=article)
 
