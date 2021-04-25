8ad58d166e1664b802e7ab6ae7390eb48458bb02
jiri.fait <fait@orkasolutions.cz>
2021-04-26 00:22:04 +0200

    Pridano SQLAlchemy

--- configs/development.py
+++ configs/development.py
@@ -1,2 +1,4 @@
 DEBUG = True
-DATABASE = "/home/spravce/Python/flask_project/blog.db"
+# DATABASE = "/home/spravce/Python/flask_project/blog.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:////home/spravce/Python/flask_project/blog.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- mdblog/app.py
+++ mdblog/app.py
@@ -1,6 +1,4 @@
 import os
-import sqlite3
-import os
 
 from flask import Flask
 from flask import render_template
@@ -18,6 +16,10 @@ from wtforms import PasswordField
 from wtforms import TextAreaField
 from wtforms.validators import InputRequired
 
+from .models import db
+from .models import Article
+
+
 flask_app = Flask(__name__)
 
 flask_app.config.from_pyfile("/home/spravce/Python/flask_project/configs/default.py")
@@ -25,6 +27,7 @@ flask_app.config.from_pyfile("/home/spravce/Python/flask_project/configs/default
 if "MDBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MDBLOG_CONFIG")
 
+db.init_app(flask_app)
 
 ## FORMS
 
@@ -59,6 +62,12 @@ def view_admin():
 
 
 ## ARTICLES
+@flask_app.route("/articles/", methods=["GET"])
+def view_articles():
+    articles = Article.query.order_by(Article.id.desc())
+    return render_template("articles_page.jinja", articles=articles)
+
+
 @flask_app.route("/articles/new/", methods=["GET"])
 def view_add_article():
     if "logged" not in session:
@@ -68,32 +77,30 @@ def view_add_article():
     return render_template("article_editor.jinja", form=form)
 
 
-@flask_app.route("/articles/", methods=["GET"])
-def view_articles():
-    db = get_db()
-    cur = db.execute("select * from articles order by id desc")
-    articles = cur.fetchall()
-    return render_template("articles_page.jinja", articles=articles)
-
-
 @flask_app.route("/articles/", methods=["POST"])
 def add_article():
     if "logged" not in session:
         return redirect(url_for(view_login))
 
-    db = get_db()
-    cur = db.execute("insert into articles (title, content) values (?,?)",
-                     [request.form.get("title"), request.form.get("content")])
-    db.commit()
-    flash("Prispevek uspesne pridan", "alert-success")
-    return redirect(url_for("view_articles"))
+    add_form = ArticleForm(request.form)
+    if add_form.validate():
+        new_article = Article(
+            title=add_form.title.data,
+            content=add_form.content.data
+        )
+        db.session.add(new_article)
+        db.session.commit()
+        flash("Prispevek uspesne pridan", "alert-success")
+        return redirect(url_for("view_articles"))
+    else:
+        for error in add_form.errors:
+            flash("{} is missing".format(error), "alert-danger")
+        return render_template("article_editor.jinj", form=add_form)
 
 
 @flask_app.route("/article/<int:art_id>")
 def view_article(art_id):
-    db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
-    article = cur.fetchone()
+    article = Article.query.filter_by(id=art_id).first()
     if article:
         return render_template("article_page.jinja", article=article)
     return render_template("article_not_found.jinja", art_id=art_id)
@@ -103,13 +110,11 @@ def view_article(art_id):
 def view_article_editor(art_id):
     if "logged" not in session:
         return redirect(url_for(view_login))
-    db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
-    article = cur.fetchone()
+    article = Article.query.filter_by(id=art_id).first()
     if article:
         form = ArticleForm()
-        form.title.data = article["title"]
-        form.content.data = article["content"]
+        form.title.data = article.title
+        form.content.data = article.content
         return render_template("article_editor.jinja", form=form, article=article)
     return render_template("article_not_found.jinja", art_id=art_id)
 
@@ -118,15 +123,14 @@ def view_article_editor(art_id):
 def edit_article(art_id):
     if "logged" not in session:
         return redirect(url_for(view_login))
-    db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
-    article = cur.fetchone()
+    article = Article.query.filter_by(id=art_id).first()
     if article:
         edit_form = ArticleForm(request.form)
         if edit_form.validate():
-            db.execute("update articles set title=?, content=? where id=?",
-                       [edit_form.title.data, edit_form.content.data, art_id])
-            db.commit()
+            article.title = edit_form.title.data
+            article.content = edit_form.content.data
+            db.session.add(article)
+            db.session.commit()
             flash("Zmeny byly ulozene", "alert-success")
             return redirect(url_for("view_article", art_id=art_id))
         else:
@@ -165,28 +169,8 @@ def logout_user():
     return redirect(url_for("view_welcome_page"))
 
 
-## UTILS
-def connect_db():
-    rv = sqlite3.connect(flask_app.config["DATABASE"])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def get_db():
-    if not hasattr(g, "sqlite.db"):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@flask_app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, "sqlite.db"):
-        g.sqlite_db.close()
-
-
+## CLI COMMANDS
 def init_db(app):
     with app.app_context():
-        db = get_db()
-        with open("mdblog/schema.sql", "r") as fp:
-            db.cursor().executescript(fp.read())
-        db.commit()
+        db.create_all()
+        print("Database inicialized")
