369b865d71051b0a8e052e309dc9d558e60a3abe
jiri.fait <fait@orkasolutions.cz>
2021-04-27 13:20:23 +0200

    Posledni verye pred Cellery

---
+++ alembic/versions/f09cf814ab15_add_newsletter_modify_db.py
@@ -0,0 +1,24 @@
+"""Add newsletter modify db
+
+Revision ID: f09cf814ab15
+Revises: 43e620430aa1
+Create Date: 2021-04-27 00:02:00.397736
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f09cf814ab15'
+down_revision = '43e620430aa1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
